160d0ba00564a01408e13a92b241c0228a666695
Mohamed Gamal <mg8033667@gmail.com>
2023-11-21 14:10:53 -0800

    fixing selection sort files

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,43 +1,35 @@
 #include "sort.h"
 
 /**
- * swap - function to swap two values
- * @x: takes the first num
- * @y: takes the second num
- * Return: Nothing
-*/
-void swap(int *x, int *y)
-{
-	int temp;
-
-	temp = *x;
-	*x = *y;
-	*y = temp;
-}
-
-/**
- * selection_sort - function to sort the number
- * by selection algorithm
+ * selection_sort - function that sorts using
+ * slelection algorithm
  * @array: takes the array
  * @size: takes the size of the array
  * Return: Nothing
 */
+
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_index;
-	/*after decleration of varaiables*/
+	size_t i, j, k;
+	int tmp;
+	/*after decleration of vrariables*/
 	if (!array || !size)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		min_index = i;
-		/*checking the smallest number between the group*/
-		for (j = i + 1; j < size; j++)
+		for (j = size - 1, k = i + 1; j > i; j--)
 		{
-			if (array[j] < array[min_index])
-				min_index = j;
+			if (array[j] < array[k])
+			{
+				k = j;
+			}
 		}
-		swap(&array[i], &array[min_index]);
-		print_array(array, size);
+		if (array[i] > array[k])
+		{
+			tmp = array[i];
+			array[i] = array[k];
+			array[k] = tmp;
+			print_array(array, size);
+		}
 	}
 }
